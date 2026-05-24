9a7c9cde03922e3b77ba0fe7310e75c692ccf7b9
DevCulture XII <windiseptian6@gmail.com>
2026-05-24 21:19:24 +0700

    Add files/uninstall.sh

---
+++ files/uninstall.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+echo ""
+echo "#########################################################"
+echo "## -> DEVCULTUR EXBIT-Server by DEVCULTUR-ID Software       ##"
+echo "## -> Author : DevCultur Exbit                           ##"
+echo "## -> Email : devculturlegend9999@gmail.com               ##"
+echo "## -> Wa    :                              ##"
+echo "## -> (c) 2023-2024, DevCultur Exbit-ID Software         ##"
+echo "#########################################################"
+echo ""
+
+BIN=/usr/bin
+CONFIGS=/etc/noobzvpns
+SYSTEMD=/etc/systemd/system
+SYSTEMCTL=$(which systemctl)
+
+if [ `id -u` != "0" ]; then
+    echo "Error at uninstallation, please run uninstaller as root"
+    exit 1
+fi
+
+echo "Uninstalling NoobzVpn-Server..."
+if [ -f $SYSTEMD/noobzvpns.service ]; then
+    $SYSTEMCTL stop noobzvpns.service
+    $SYSTEMCTL disable noobzvpns.service
+    rm $SYSTEMD/noobzvpns.service
+    $SYSTEMCTL daemon-reload
+fi
+if [ -f $BIN/noobzvpns ]; then
+    rm $BIN/noobzvpns
+fi
+if [ -d $CONFIGS ]; then
+    rm -rf $CONFIGS
+fi
+echo "Uninstall NoobzVpn-Server completed"
