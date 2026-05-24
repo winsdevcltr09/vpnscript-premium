8fc188c295e33325f61ce269570a579783bb2b72
DevCulture XII <windiseptian6@gmail.com>
2026-05-24 21:39:01 +0700

    Update udp-custom/udp-custom.sh

--- udp-custom/udp-custom.sh
+++ udp-custom/udp-custom.sh
@@ -6,7 +6,7 @@ mkdir -p /etc/udp
 echo "change to time GMT+7"
 ln -fs /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
 cd /etc/udp
-wget -q -O udp-custom "https://raw.githubusercontent.com/devculturlegend9999/alpha/main/udp-custom/udp-custom-linux-amd64"
+wget -q -O udp-custom "https://raw.githubusercontent.com/winsdevcltr09/vpnscript-premium/main/udp-custom/udp-custom-linux-amd64"
 chmod +x udp-custom
 cd
 
